981597f384ddafd3f37d3ad17da0a5ca6e20d6f4
Ryan Behari <ryan.behari04@gmail.com>
2023-12-31 18:58:37 +0100

    Assignment 3 start

---
+++ assignment 3/assignment3.py
@@ -0,0 +1,12 @@
+#Ryan Behari
+#s3618765
+#Assignment 3 Type Checker
+#Universiteit Leiden
+#Assignment 1+2 code is reused for this assignment#
+
+import sys
+import copy
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+           'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
